35d415d9bd5e67b0b578e180d09c04cf12ab7d12
Lazuli Ziloet <rneutron@protonmail.com>
2023-04-19 16:43:59 +0300

    make more readable

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
 @echo off
 
-if not exist "build" mkdir "build"
+if not exist "build" (
+	mkdir "build"
+)
 pushd "build"
 set CompilerFlags=/nologo /W3 /Z7 /Fe:"main.exe"
 set LinkerFlags=/incremental:no /opt:icf /opt:ref /subsystem:windows
