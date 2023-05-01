c1da1353d02798b4b18b3893191886250be822c6
Lazuli Ziloet <rneutron@protonmail.com>
2023-05-01 22:01:15 +0300

    Rect is now a Cube

--- main.c
+++ main.c
@@ -234,7 +234,7 @@ void WinMainCRTStartup()
 
 	glBindVertexArray(VertexArray);
 	glBindBuffer(GL_ARRAY_BUFFER, VertexBuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Rect), Rect, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Cube), Cube, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), 0);
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(0);
