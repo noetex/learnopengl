c88c01e77c9fe169020d21c97ddcaf225648d3d5
Lazuli Ziloet <rneutron@protonmail.com>
2023-05-03 12:54:31 +0300

    treat linker warnings as errors

--- build.bat
+++ build.bat
@@ -6,7 +6,7 @@ if not exist "build" (
 )
 pushd "build"
 set CompilerFlags=/nologo /W3 /Z7 /Fe:"main.exe"
-set LinkerFlags=/incremental:no /opt:icf /opt:ref /subsystem:windows
+set LinkerFlags=/incremental:no /opt:icf /opt:ref /subsystem:windows /WX
 set LibsToLink=kernel32.lib user32.lib gdi32.lib opengl32.lib libucrt.lib
 call cl %CompilerFlags% ../main.c /link %LinkerFlags% %LibsToLink%
 popd
