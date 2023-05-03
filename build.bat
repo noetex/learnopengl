cc3addeb0986aad2115dd895c8c3576fd648aa58
Lazuli Ziloet <rneutron@protonmail.com>
2023-05-03 13:00:02 +0300

    use /Zi flag instead of /Z7

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ if not exist "build" (
 	mkdir "build"
 )
 pushd "build"
-set CompilerFlags=/nologo /W3 /Z7 /Fe:"main.exe"
+set CompilerFlags=/nologo /W3 /Zi /Fe:"main.exe"
 set LinkerFlags=/incremental:no /opt:icf /opt:ref /subsystem:windows /WX
 set LibsToLink=kernel32.lib user32.lib gdi32.lib opengl32.lib libucrt.lib
 call cl %CompilerFlags% ../main.c /link %LinkerFlags% %LibsToLink%
