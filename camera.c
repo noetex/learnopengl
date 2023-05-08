d3be3a7962b5ce37095c63da9dcc29725c6d8219
Lazuli Ziloet <rneutron@protonmail.com>
2023-05-08 19:22:52 +0300

    cleanup: remove unused code

--- camera.c
+++ camera.c
@@ -8,21 +8,6 @@ typedef struct
 	float Pitch;
 } camera;
 
-#if 0
-static matrix4
-CameraView(camera Camera)
-{
-	matrix4 Translation = Matrix4_Translate(Vector3_Scale(Camera.Position, -1));
-	matrix4 Transform;
-	Transform.AxisX = (vector4){Camera.Right.X, Camera.Up.X, Camera.Front.X, 0};
-	Transform.AxisY = (vector4){Camera.Right.Y, Camera.Up.Y, Camera.Front.Y, 0};
-	Transform.AxisZ = (vector4){Camera.Right.Z, Camera.Up.Z, Camera.Front.Z, 0};
-	Transform.AxisW = Vector4_UnitW();
-	matrix4 Result = Matrix4_MultiplyMatrix4(Transform, Translation);
-	return Result;
-}
-#endif
-
 static vector3
 Camera_AxisX(camera Camera)
 {
